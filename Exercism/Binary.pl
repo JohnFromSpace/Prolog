c603b16f9b56e4c595056c94f1b3e2695ce5bccd
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-24 00:54:34 +0200

    Update Binary.pl

--- Exercism/Binary.pl
+++ Exercism/Binary.pl
@@ -1,1 +1,4 @@
-
+binary(Str, Dec) :- string_chars(Str, Chars), chars_binary(Chars, Dec).
+chars_binary([], 0).
+chars_binary(['0'|T], Dec) :- chars_binary(T, Dec), !.
+chars_binary(['1'|T], Dec2) :- length(T, Pow), chars_binary(T, TDec), Dec2 is 2**Pow + TDec. 
