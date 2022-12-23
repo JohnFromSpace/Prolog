9643ce195cb1a92a895a415deb9929a317cf7d9b
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-24 01:29:07 +0200

    Update Anagram.pl

--- Exercism/Anagram.pl
+++ Exercism/Anagram.pl
@@ -1,3 +1,5 @@
 anagram(Word, Options, Matching) :- include(anagrams(Word), Options, Matching).
 
 key(Word, Lower, Sorted) :- string_lower(Word, Lower), atom_chars(Lower, Chars), msort(Chars, Sorted).
+
+anagrams(Word1, Word2) :- key(Word1, Lower1, Sorted), key(Word2, Lower2, Sorted), \+ Lower1 == Lower2.
