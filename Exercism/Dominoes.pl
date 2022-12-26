dbeb2c87da062d49add52d213cf8e41e909e8fdb
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-26 18:19:22 +0200

    Update Dominoes.pl

--- Exercism/Dominoes.pl
+++ Exercism/Dominoes.pl
@@ -2,3 +2,4 @@ can_chain([]).
 can_chain([(A, B) | R]) :- once(can_chain(A, B, R)).
 
 can_chain(X, X, []).
+can_chain(S, Pr, Pl) :- select(P, Pl, R), ((Pr, N) = P ; (N, Pr) = P), can_chain(S, N, R).
