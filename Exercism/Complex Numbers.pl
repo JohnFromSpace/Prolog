d96153d43480b9b58a99439b0e3474502ad631ce
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-26 19:01:52 +0200

    Update Complex Numbers.pl

--- Exercism/Complex Numbers.pl
+++ Exercism/Complex Numbers.pl
@@ -2,3 +2,5 @@ real((Real, _), Real).
 imaginary((_, Imaginary), Imaginary).
 
 conjugate((Real, Imaginary), (Real, Complex)) :- Complex is -1 * Imaginary.
+
+abs((Real, Imaginary), A) :- A is sqrt(Real * Real + Imaginary * Imaginary).
