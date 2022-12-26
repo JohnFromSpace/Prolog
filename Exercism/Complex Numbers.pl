825e9b99da3152284d5c8311ff4ea66be28f90e6
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-26 19:02:24 +0200

    Update Complex Numbers.pl

--- Exercism/Complex Numbers.pl
+++ Exercism/Complex Numbers.pl
@@ -8,3 +8,5 @@ abs((Real, Imaginary), A) :- A is sqrt(Real * Real + Imaginary * Imaginary).
 add((Real1, Imaginary1), (Real2, Imaginary2), (RReal, RImaginary)) :- RReal is Real1 + Real2, RImaginary is Imaginary1 + Imaginary2.
 
 sub((Real1, Imaginary1), (Real2, Imaginary2), (RReal, RImaginary)) :- RReal is Real1 - Real2, RImaginary is Imaginary1 - Imaginary2.
+
+mul((Real1, Imaginary1), (Real2, Imaginary2), (RReal, RImaginary)) :- RReal is Real1 * Real2 - Imaginary1 * Imaginary2, RImaginary is Imaginary1 * Real2 + Imaginary2 * Real1.
