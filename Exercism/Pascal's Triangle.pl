16b4b8ac6417db9282de2d77fc91f37c7e6797ad
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-26 04:01:18 +0200

    Update Pascal's Triangle.pl

--- Exercism/Pascal's Triangle.pl
+++ Exercism/Pascal's Triangle.pl
@@ -7,3 +7,4 @@ sum_by_pairs([A, B|Ns], [S|T]) :- S is A+B, sum_by_pairs([B|Ns], T).
 pascal(0, _, []) :- !.
 pascal(N, R, [RR|Rs]) :- NN is N-1, next_row(R, RR), pascal(NN, RR, Rs).
 
+pascal(N, T) :- pascal(N, [], T).
