8bc397b2dce395485d17522f8be97e8c728d9337
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-26 03:59:45 +0200

    Update Pascal's Triangle.pl

--- Exercism/Pascal's Triangle.pl
+++ Exercism/Pascal's Triangle.pl
@@ -1,3 +1,5 @@
 next_row([], [1]) :- !.
 next_row(R, RR) :- sum_by_pairs(R, R1), append([1|R1], [1], RR).
 
+sum_by_pairs([_], []) :- !.
+sum_by_pairs([A, B|Ns], [S|T]) :- S is A+B, sum_by_pairs([B|Ns], T).
