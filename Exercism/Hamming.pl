c8bf53490fc0925a30e5326b5c6763c28a76bdc3
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-28 01:51:44 +0200

    Update Hamming.pl

--- Exercism/Hamming.pl
+++ Exercism/Hamming.pl
@@ -1,1 +1,1 @@
-hamming_distance(Str1, Str2, Dist).
+hamming_distance(Str1, Str2, Dist)  :- string_codes(Str1, Codes1), string_codes(Str2, Codes2), foldl([A, B, S, R] >> (A =:= B -> R is S; R is S + 1), Codes1, Codes2, 0, Dist)..
