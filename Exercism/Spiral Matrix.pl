e120d1830a66a81a5d1c46cd8963383565ed3913
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-29 00:54:22 +0200

    Update Spiral Matrix.pl

--- Exercism/Spiral Matrix.pl
+++ Exercism/Spiral Matrix.pl
@@ -4,3 +4,4 @@ spiral(N, XS) :- spiral_H(N, N, 1, XS).
 rest([_|X], X).
 
 transpose(XS, [])   :- member([], XS), !.
+transpose(M, [X|T]) :- maplist(nth0(0), M, X), maplist(rest, M, M1), transpose(M1, T).
