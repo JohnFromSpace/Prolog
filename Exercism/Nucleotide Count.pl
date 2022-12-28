c6c00b7394df6ec23908a913057f4db77e4e5374
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-28 23:15:59 +0200

    Update Nucleotide Count.pl

--- Exercism/Nucleotide Count.pl
+++ Exercism/Nucleotide Count.pl
@@ -1,1 +1,3 @@
 char_count(Chars, Char, Count):- aggregate_all(count, (member(X, Chars), X == Char), Count).
+
+nucleotide_count(Str, Counts):- string_chars(Str, Chars), forall(member(X, Chars), (X == 'A'; X == 'C'; X == 'G'; X == 'T')), char_count(Chars, 'A', A), char_count(Chars, 'C', C), char_count(Chars, 'G', G), char_count(Chars, 'T', T), Counts = [('A', A), ('C', C), ('G', G), ('T', T)].
