cc958f09ece663d664ad87a5ae6923e49da767d6
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-29 00:20:47 +0200

    Update Satellite.pl

--- Exercism/Satellite.pl
+++ Exercism/Satellite.pl
@@ -3,3 +3,5 @@ inorder(node(L, N, R)) --> inorder(L), [N], inorder(R).
 
 preorder(nil) --> [].
 preorder(node(L, N, R)) --> [N], preorder(L), preorder(R).
+
+tree_traversals(T, P, I) :- phrase(preorder(T), P), phrase(inorder(T), I).
