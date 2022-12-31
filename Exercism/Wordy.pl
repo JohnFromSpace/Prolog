4f1f9d053b97265cb7d593c09fd1243e68bb1982
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-31 02:16:47 +0200

    Update Wordy.pl

--- Exercism/Wordy.pl
+++ Exercism/Wordy.pl
@@ -1,6 +1,6 @@
 :- use_module(library(dcg/basics)).
 
-wordy(Question, Answer) :- string_codes(Question, Codes), phrase(parse(FirstNumber, OperandNumbers), Codes, []), foldl(calculate, OpNumbers, FirstNumber, Answer).
+wordy(Question, Answer) :- string_codes(Question, Codes), phrase(parse(FirstNumber, OperandNumbers), Codes, []), foldl(calculate, OperandNumbers, FirstNumber, Answer).
 
 calculate(("+", A), B, C) :- C is B + A.
 calculate(("-", A), B, C) :- C is B - A.
