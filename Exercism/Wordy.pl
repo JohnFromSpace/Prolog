46011babc477a6efcaa7dd1bc9b067be4dfcd359
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-31 02:17:21 +0200

    Update Wordy.pl

--- Exercism/Wordy.pl
+++ Exercism/Wordy.pl
@@ -7,6 +7,6 @@ calculate(("-", A), B, C) :- C is B - A.
 calculate(("*", A), B, C) :- C is B * A.
 calculate(("/", A), B, C) :- C is B / A.
 
-parse(FirstNumber, OpNums) --> question, first_number(FirstNumber), op_nums(OperandNums), "?", !.
+parse(FirstNumber, OpNums) --> question, first_number(FirstNumber), operand_numbers(OperandNums), "?", !.
 
 question --> "What".
