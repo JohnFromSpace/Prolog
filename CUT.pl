55a3619377649d999691a0fdc635440cb4c4315a
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2023-12-31 19:13:34 +0200

    Update CUT.pl

--- CUT.pl
+++ CUT.pl
@@ -1,1 +1,20 @@
-C
+% In Prolog, the cut (!) is a control construct used to commit to the choices made during the execution of a program. 
+% It prunes the search space and prevents backtracking to choices made before the cut. 
+% The cut is a powerful tool, but it should be used with caution, as it can make the code less readable and harder to understand.
+
+% Here's a simple example to illustrate the use of the cut:
+
+/* Define a predicate to classify a number as positive, negative, or zero */
+classify_number(X, positive) :- X > 0, !.
+classify_number(X, zero)     :- X =:= 0, !.
+classify_number(X, negative) :- X < 0.
+
+/* Example usage */
+?- classify_number(5, Class).
+% Output: Class = positive
+
+?- classify_number(0, Class).
+% Output: Class = zero
+
+?- classify_number(-3, Class).
+% Output: Class = negative
