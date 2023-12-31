9e7baab3aaebf90d679f04476e6010644dee463c
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2023-12-31 18:27:55 +0200

    Update Lists.pl

--- Lists.pl
+++ Lists.pl
@@ -8,3 +8,6 @@
 [[1,2,3,4], p, this] % 3 element list 
 [[[1, 2], [3, 4]], [[a, b], [x, y]]].    
 % nested list (3 level nesting)
+
+% In prolog, lists have got only one operator, called pipe, denoted by |. 
+% This operator is used to append an element at the beginning of a list. The syntax of the pipe operator is as follows:
