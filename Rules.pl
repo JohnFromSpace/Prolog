eba1695560637737bcb5d740614099d14e042d9a
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2023-12-31 17:59:54 +0200

    Update Rules.pl

--- Rules.pl
+++ Rules.pl
@@ -5,3 +5,9 @@
 % T :- T. 
 % F :- F.
 
+% Let's look at the following examples:
+human(Einstein). % Einstein is a human. 
+
+% This can be turned to a rule the following way:
+human(Einstein) :- true.
+
