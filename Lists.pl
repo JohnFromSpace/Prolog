549ba2366b002931c207c64ef1d2cdc88966c8c8
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2023-12-31 18:24:42 +0200

    Update Lists.pl

--- Lists.pl
+++ Lists.pl
@@ -2,3 +2,9 @@
 % In Prolog, lists are inbuilt data structures. 
 % Lists can be used to represent sets, stacks, queues, linked lists, and several complex data structures such as trees, graphs, etc.
 
+[]            % empty list
+[a]            % singleton list
+[hello, world]        % 2 element list
+[[1,2,3,4], p, this] % 3 element list 
+[[[1, 2], [3, 4]], [[a, b], [x, y]]].    
+% nested list (3 level nesting)
