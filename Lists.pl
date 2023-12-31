957d1c3849e7b049a2442a15948f017b1944fecd
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2023-12-31 18:23:39 +0200

    Update Lists.pl

--- Lists.pl
+++ Lists.pl
@@ -1,1 +1,4 @@
+% A list is a collection of items, not necessarily homogeneous. 
+% In Prolog, lists are inbuilt data structures. 
+% Lists can be used to represent sets, stacks, queues, linked lists, and several complex data structures such as trees, graphs, etc.
 
