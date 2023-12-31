c9e05e8567c45ca18aa2bd97df42f23007aed9b8
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2023-12-31 18:57:40 +0200

    Update Infinite Loops.pl

--- Infinite Loops.pl
+++ Infinite Loops.pl
@@ -1,1 +1,8 @@
+% Infinite Loops occur in two cases:
+% Case 1):
+Parent(X, Y) :- Child(Y, X). % X is a parent of a Y, only if Y is a child of X.
+Child(A, B) :- Child(B, A).
+
+?- Parent(Ivan, Bob). % Infinite loop
+?- Child(Bob, Ivan). % Infinite loop
 
