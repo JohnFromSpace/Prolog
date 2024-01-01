186d9a62c5a6f3bdfca985f934ef80f3b8d0643e
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-01 06:39:42 +0200

    Update Strings.pl

--- Strings.pl
+++ Strings.pl
@@ -18,3 +18,21 @@ is_string_atom(Term) :-
 ?- is_string_atom('Prolog').
 % true
 
+% Examples of string representation using lists of characters
+string_list(['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']).
+string_list(['P', 'r', 'o', 'l', 'o', 'g', ' ', 'i', 's', ' ', 'f', 'u', 'n', '!']).
+
+% Predicate to check if a term is a list representing a string
+is_string_list(Term) :-
+    is_list(Term),
+    maplist(char_type(_, alpha), Term).
+
+% Example usage
+?- is_string_list(['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']).
+% true
+
+?- is_string_list([49, 50, 51]).
+% false (contains numeric character codes)
+
+?- is_string_list(['P', 'r', 'o', 'l', 'o', 'g', ' ', 'i', 's', ' ', 'f', 'u', 'n', '!']).
+% true
