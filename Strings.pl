64b974d5ca99dcf98b03e0798ac24fc222dd4b11
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-01 06:39:13 +0200

    Update Strings.pl

--- Strings.pl
+++ Strings.pl
@@ -1,1 +1,20 @@
-S
+% Examples of string representation using atoms
+string_atom('Hello World').
+string_atom('Prolog is fun!').
+
+% Predicate to check if a term is an atom representing a string
+is_string_atom(Term) :-
+    atom(Term),
+    atom_chars(Term, Chars),
+    maplist(char_type(_, alpha), Chars).
+
+% Example usage
+?- is_string_atom('Hello World').
+% true
+
+?- is_string_atom('123').
+% false (contains numeric characters)
+
+?- is_string_atom('Prolog').
+% true
+
